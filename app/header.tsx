4714e6054ea6131ae0e451787016b0a5c7599d28
KoushaAmouzesh <kousha82@@students.cs.ubc.ca>
2023-06-08 13:30:24 -0700

    good changes

--- app/header.tsx
+++ app/header.tsx
@@ -1,18 +1,72 @@
-import React from "react";
-import "../styles/head.css"
-
+'use client';
+import React, { useState, useEffect } from "react";
+import "../styles/head.css";
 
 const Header: React.FC = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  const scrollToSection = (event: React.MouseEvent<HTMLAnchorElement>, id: string) => {
+    event.preventDefault();
+    const section = document.getElementById(id);
+    if (section) {
+      const headerHeight = document.querySelector('header')?.clientHeight || 0;
+      window.scrollTo({
+        top: section.offsetTop - headerHeight,
+        behavior: 'smooth',
+      });
+      setIsSidebarOpen(false);
+    }
+  };
+
+  
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+        // not sure about the proper width yetttt
+      setIsMobileView(window.innerWidth <= 768); 
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <header className="transparent-header">
-      <nav>
+    <header className={`transparent-header ${isSidebarOpen && isMobileView ? "sidebar-open" : ""}`}>
+      {isMobileView ? (
+        <div className="sidebar-toggle" onClick={toggleSidebar}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      ) : (
+        <nav className="transparent-header-nav">
+            <ul>
+                <li><a href="#AboutMe" onClick={(e) => scrollToSection(e, 'AboutMe')}>Me</a></li>
+                <li> <a href="#Acedamics" onClick={(e) => scrollToSection(e, 'Acedamics')}>Education</a></li>
+                <li><a href="#Skills" onClick={(e) => scrollToSection(e, 'Skills')}>Skills</a></li>
+                <li><a href="#Experience" onClick={(e) => scrollToSection(e, 'Experience')}>Experience</a></li>
+                <li><a href="#Projects" onClick={(e) => scrollToSection(e, 'Projects')}>Projects</a></li>
+                <li><a href="#Contact" onClick={(e) => scrollToSection(e, 'Contact')}>Contact</a></li>
+            </ul>
+        </nav>
+      )}
+      <nav className={`sidebar ${isSidebarOpen && isMobileView ? "open" : ""}`}>
         <ul>
-          <li><a href="#AboutMe">Me</a></li>
-          <li><a href="#Acedamics">Education</a></li>
-          <li><a href="#Skills">Skills</a></li>
-          <li><a href="#Experience">Experience</a></li>
-          <li><a href="#Projects">Projects</a></li>
-          <li><a href="#Contact">Contact</a></li>
+            <li><a href="#AboutMe" onClick={(e) => scrollToSection(e, 'AboutMe')}>Me</a></li>
+            <li> <a href="#Acedamics" onClick={(e) => scrollToSection(e, 'Acedamics')}>Education</a></li>
+            <li><a href="#Skills" onClick={(e) => scrollToSection(e, 'Skills')}>Skills</a></li>
+            <li><a href="#Experience" onClick={(e) => scrollToSection(e, 'Experience')}>Experience</a></li>
+            <li><a href="#Projects" onClick={(e) => scrollToSection(e, 'Projects')}>Projects</a></li>
+            <li><a href="#Contact" onClick={(e) => scrollToSection(e, 'Contact')}>Contact</a></li>
         </ul>
       </nav>
     </header>
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,45 @@
-import Image from 'next/image'
+import React from 'react';
+import Header from './header';
+import '../styles/page.css'; // Import the CSS file for the Home component
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <Header />
+      <section id="AboutMe" className="section">
+        <div>
+          <h1>Kousha Amouzesh</h1>
+          <p>I am a 20-year-old Computer Science student at Simon Fraser University in Burnaby, British Columbia. Since I was young, I have been fascinated by the potential of computer science and mathematics, beginning by the first space ship game I made at 14. As I advance in my academic journey, I am consistently inspired by the projects that connect the endless world of mathematics with computer science. I am currently looking for a co-op position in the remainder of 2023.</p>
+          <p>Aside from my passion for technology and academia, I am highly competitive and dedicated to sports. I have pursued competitive swimming for 8 years. Moreover, I have been actively engaged in recreational soccer, enjoying the complexity and thrill of the game. Besides, I grew up with musical instruments in hand, and I play the Persian Tombak, a bit of piano and drums.</p>
         </div>
-      </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section id="Acedamics" className="section">
+        <h2>Education</h2>
+        {/* Your content for the Education section */}
+      </section>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <section id="Skills" className="section">
+        <h2>Skills</h2>
+        {/* Your content for the Skills section */}
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <section id="Experience" className="section">
+        <h2>Experience</h2>
+        {/* Your content for the Experience section */}
+      </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+      <section id="Projects" className="section">
+        <h2>Projects</h2>
+        {/* Your content for the Projects section */}
+      </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section id="Contact" className="section">
+        <h2>Contact</h2>
+        {/* Your content for the Contact section */}
+      </section>
     </main>
-  )
-}
+  );
+};
+
+export default Home;
