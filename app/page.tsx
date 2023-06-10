8041e2fc6bf70737f924e1d810eef941816105fe
KoushaAmouzesh <kousha82@@students.cs.ubc.ca>
2023-06-09 20:51:42 -0700

    some changes on intro: todo projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,68 +1,67 @@
 'use client';
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Header from './header';
 import '../styles/page.css'; // Import the CSS file for the Home component
 
 const Home: React.FC = () => {
+
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+  
   return (
     <main>
       <Header />
       <section className="section">
         <div className="wrapper">
           <div className="typing-demo">Kousha Amouzesh</div>
+          <div className={`typing-demo-2 ${isVisible ? 'visible' : ''}`}>
+            I am a software developer
+          </div>
+
         </div>
+        
       </section>
       <section id="AboutMe" className="section">
-        <h1>
-          <strong>About me...</strong>
-        </h1>
+        <h1 style ={{marginLeft: "80px", fontSize: "60px"}}><strong>About me...</strong></h1>
         <div className="about-content">
-          <div className="about-text">
+          <div className="about-text" style={{ margin:"40px" }}>
             <p className="about-paragraph">
-              I am a 20-year-old Computer Science student at Simon Fraser University in Burnaby, British Columbia. Since I
-              was young, I have been fascinated by the potential of computer science and mathematics, beginning with the
-              first space ship game I made at 14. As I advance in my academic journey, I am consistently inspired by the
-              projects that connect the endless world of mathematics with computer science. I am currently looking for a
-              co-op position in the remainder of 2023.
+              I am a 20-year-old Computer Science student at Simon Fraser University in Burnaby, British Columbia. Since a young age, I have been fascinated by the potential of computer science and mathematics, starting with the first spaceship game I developed at the age of 14. As I progress in my academic journey, I am constantly inspired by projects that bridge the vast world of mathematics with computer science. Currently, I am actively seeking a co-op position for the remainder of 2023.
             </p>
             <p className="about-paragraph">
-              Aside from my passion for technology and academia, I am highly competitive and dedicated to sports. I have
-              pursued competitive swimming for 8 years. Moreover, I have been actively engaged in recreational soccer,
-              enjoying the complexity and thrill of the game. Besides, I grew up with musical instruments in hand, and I
-              play the Persian Tombak, a bit of piano and drums.
+              Besides my passion for technology and academia, I have a strong drive for sports and thrive in competitive environments. I have been involved in competitive swimming for 8 years and also enjoy the complexity and excitement of recreational soccer. Additionally, I have a musical background and play the Persian Tombak, piano, and drums.
             </p>
           </div>
           <img
             src="https://github.com/KoushaAm/Portfolio/blob/master/Screenshot%202023-05-28%20at%209.43.43%20PM.png?raw=true"
             alt="About Me"
             className="about-image"
+            style={{ width: "400px", height: "auto" , marginLeft: "40px", marginRight:"40px"}}
           />
         </div>
       </section>
 
-      <section id="Acedamics" className="section">
+      <section id="Academics" className="section">
         <div className="education-content" style={{ lineHeight: "1.2" }}>
-          <h1 style={{ textAlign: 'center' }}>
-            <strong>Education</strong>
-          </h1>
+          <h1 style={{ textAlign: 'center' }}><strong>Education</strong></h1>
           <div className="education-item">
-            <p style={{ marginLeft: '20px', fontSize: '25px', textAlign: 'left' }}>
-              <strong>Computer Science BSc</strong>
-            </p>
+            <p style={{ marginLeft: '20px', fontSize: '25px', textAlign: 'left' }}><strong>Computer Science BSc</strong></p>
             <p style={{ marginLeft: '50px', textAlign: 'left' }}>Simon Fraser University</p>
             <p style={{ marginLeft: '50px', textAlign: 'left' }}>GPA: (A) 3.92/4.33</p>
             <p style={{ marginLeft: '50px', textAlign: 'left' }}>Expected Graduation: December 2025</p>
           </div>
           <div className="education-item" style={{ lineHeight: "1.2" }}>
-            <h1 style={{marginTop:'0px',  marginLeft: '20px', textAlign: 'left' }}>Academic Objectives</h1>
-            <p style={{ marginTop:'0px', marginLeft: '20px', textAlign: 'left' }}>
-              My academic objectives are to pursue a graduate degree in Computer Science focused upon Machine learning
-              studies.
-            </p>
-            <p style={{ marginTop:'0px', marginLeft: '20px', textAlign: 'left' }}>
-              I plan to accomplish this goal after acquiring hands-on skills in industry through co-op and full-time
-              software development/engineering positions.
-            </p>
+            <h1 style={{ marginTop: '0px', marginLeft: '20px', textAlign: 'left' }}>Academic Objectives</h1>
+            <p style={{ marginTop: '0px', marginLeft: '20px', textAlign: 'left' }}>My academic objectives are to pursue a graduate degree in Computer Science with a focus on Machine Learning studies.</p>
+            <p style={{ marginTop: '0px', marginLeft: '20px', textAlign: 'left' }}>I plan to achieve this goal after gaining practical industry skills through co-op and full-time software development/engineering positions.</p>
           </div>
         </div>
       </section>
@@ -107,5 +106,3 @@ const Home: React.FC = () => {
 };
 
 export default Home;
-
-
